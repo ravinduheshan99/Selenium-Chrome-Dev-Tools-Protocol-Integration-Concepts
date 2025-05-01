c06640d998256560cbe7fd6e50c3d751de46b99a
ravinduheshan99 <ravinduheshan99@gmail.com>
2025-05-01 10:31:22 +0530

    Execute cdp command to construct the own cdp function

---
+++ src/CDPCommandsTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CDPCommandsTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Documents\\Career\\My Projects\\Selenium Projects\\01-Introduction-Project\\selenium webdriver\\ChromeDriver\\chromedriver.exe");
+
+        ChromeDriver driver = new ChromeDriver();
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+
+        Map deviceMetrics = new HashMap();
+        deviceMetrics.put("width", 600);
+        deviceMetrics.put("height", 1000);
+        deviceMetrics.put("deviceScaleFactor", 50);
+        deviceMetrics.put("mobile", true);
+        driver.executeCdpCommand("Emulation.setDeviceMetricsOverride", deviceMetrics);
+        
+        driver.get("https://rahulshettyacademy.com/angularAppdemo");
+        driver.findElement(By.cssSelector(".navbar-toggler")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.linkText("Library")).click();
+
+    }
+}
